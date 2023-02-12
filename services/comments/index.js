553b8e743b0c7eb1dff25bad917098f148933fec
nunees <felipe_silva_nunes@hotmail.com>
2023-02-11 23:20:05 -0300

    feat: add PostCreate component and add cors to service app

--- services/comments/index.js
+++ services/comments/index.js
@@ -1,23 +1,25 @@
 const express = require("express");
-const bodyParser = require('body-parser');
-const {randomBytes} = require('crypto');
+const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto");
+import cors from "cors";
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const commentsByPostId = {};
 
-app.get('/posts/:id/comments', (req, res) => {
+app.get("/posts/:id/comments", (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments', (req, res) => {
-  const commentId = randomBytes(4).toString('hex');
-  const {content} = req.body;
-  
+app.post("/posts/:id/comments", (req, res) => {
+  const commentId = randomBytes(4).toString("hex");
+  const { content } = req.body;
+
   const comments = commentsByPostId[req.params.id] || [];
 
-  comments.push({id: commentId, content});
+  comments.push({ id: commentId, content });
 
   commentsByPostId[req.params.id] = comments;
 
@@ -25,5 +27,5 @@ app.post('/posts/:id/comments', (req, res) => {
 });
 
 app.listen(4001, () => {
-  console.log("Listening on port 4001")
+  console.log("Listening on port 4001");
 });
